35bfefa855e5f114ae490285e7cad5ba98ebbeea
Mario Kellner <Mario.Kellner@studmail.w-hs.de>
2016-05-23 18:53:27 +0200

    rename config to config.tpl.php

---
+++ Config.tpl.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Mario
+ * Date: 21.05.2016
+ * Time: 11:50
+ */
+class Config
+{
+    public static $databaseSettings = [
+        'host' => 'guessandwin.gamer-point.com',
+        'user' => 'guessandwinTeam',
+        'password' => '***',
+        'database' => 'em2016'
+    ];
+}
+
+define('ROOT_DIR', __DIR__);
+define('DS', DIRECTORY_SEPARATOR);
