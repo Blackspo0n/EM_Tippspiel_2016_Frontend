bee0b57a75bacf18b5a49201a774543b4bf7cacf
PhilippMiller <philipp.miller@gmx.de>
2016-06-07 08:53:57 +0200

    edit main.class.php

--- classes/controller/main.class.php
+++ classes/controller/main.class.php
@@ -3,6 +3,17 @@
 class main implements IController {
     public function __construct()
     {
+
+        $resultSet = Application::$database->databaseLink->query("SELECT * FROM spiele WHERE gelbekarteheim=NULL");
+        $myArray = [];
+        if ($resultSet) {
+            while ($row = $resultSet->fetch_assoc()) {
+                $myArray[] = $row;
+            }
+
+            Application::$smarty->assign("SpieleArray", $myArray);
+        }
+
         Application::$smarty->assign("yolo", "You only 'lie' once.");
         Application::$smarty->assign('contentfile', 'main.tpl');
     }
